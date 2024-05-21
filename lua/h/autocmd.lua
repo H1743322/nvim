eba546c6f89de5bfbcc1fcd609d3ea66df2b8abb
H17 <61067199+H1743322@users.noreply.github.com>
2024-05-21 20:32:45 +0100

    feat: Remove lsp zero

--- lua/h/autocmd.lua
+++ lua/h/autocmd.lua
@@ -22,7 +22,7 @@ autocmd({ "BufWritePre" }, {
     command = [[%s/\s\+$//e]],
 })
 
-autocmd({"User"}, {
+autocmd({ "User" }, {
     group = h,
     pattern = "TelescopePreviewerLoaded",
     callback = function()
@@ -53,3 +53,20 @@ autocmd({ "BufRead", "BufNewFile" }, {
 --        vim.keymap.set("n", "<leader>f", "<cmd>!yarn run eslint -f node_modules/eslint-friendly-formatter src test --fix<CR>",{buffer = true})
 --    end
 --})
+autocmd('LspAttach', {
+    group = h,
+    callback = function(event)
+        local opts = { buffer = event.buf }
+        vim.keymap.set("n", "gd", function() require('telescope.builtin').lsp_definitions() end, opts)
+        vim.keymap.set("n", "gr", function() require('telescope.builtin').lsp_references() end, opts)
+        vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "gs", function() vim.diagnostic.signature_help() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("n", "<leader>vd", ':vsp<cr> :lua vim.lsp.buf.definition()<CR><CR>zz')
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
--- lua/h/lazy/lsp.lua
+++ lua/h/lazy/lsp.lua
@@ -1,21 +1,9 @@
 return {
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        lazy = true,
-        config = false,
-        init = function()
-            -- Disable automatic setup, we are doing it manually
-            vim.g.lsp_zero_extend_cmp = 0
-            vim.g.lsp_zero_extend_lspconfig = 0
-        end,
-    },
-    {
         'williamboman/mason.nvim',
         lazy = false,
         config = true,
     },
-
     -- Autocompletion
     {
         'hrsh7th/nvim-cmp',
@@ -29,12 +17,9 @@ return {
         },
         config = function()
             -- Here is where you configure the autocompletion settings.
-            local lsp_zero = require('lsp-zero')
-            lsp_zero.extend_cmp()
 
             -- And you can configure cmp even more, if you want to.
             local cmp = require('cmp')
-            local cmp_action = lsp_zero.cmp_action()
             local ls = require("luasnip")
             require('luasnip.loaders.from_vscode').lazy_load()
             vim.keymap.set({ "i", "s" }, "<C-s>;", function() ls.jump(1) end, { silent = true })
@@ -93,28 +78,6 @@ return {
             { 'williamboman/mason-lspconfig.nvim' },
         },
         config = function()
-            -- This is where all the LSP shenanigans will live
-            local lsp_zero = require('lsp-zero')
-            lsp_zero.extend_lspconfig()
-
-            --- if you want to know more about lsp-zero and mason.nvim
-            --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-            lsp_zero.on_attach(function(client, bufnr)
-                -- see :help lsp-zero-keybindings
-                -- to learn the available actions
-                lsp_zero.default_keymaps({ buffer = bufnr })
-                local opts = { buffer = bufnr, remap = false }
-
-
-                vim.keymap.set("n", "gd", function() require('telescope.builtin').lsp_definitions() end, opts)
-                vim.keymap.set("n", "gr", function() require('telescope.builtin').lsp_references() end, opts)
-                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-                vim.keymap.set("n", "<leader>vd", ':vsp<cr> :lua vim.lsp.buf.definition()<CR><CR>zz')
-            end)
-
             vim.diagnostic.config({
                 virtual_text = true,
                 severity_sort = true,
@@ -140,24 +103,40 @@ return {
                     update_in_insert = true,
 
                 })
-            require('mason-lspconfig').setup({
-                ensure_installed = {},
+
+            local cmp_lsp = require("cmp_nvim_lsp")
+            local capabilities = vim.tbl_deep_extend(
+                "force",
+                {},
+                vim.lsp.protocol.make_client_capabilities(),
+                cmp_lsp.default_capabilities())
+
+            capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                },
                 handlers = {
-                    lsp_zero.default_setup,
-                    lua_ls = function()
-                        -- (Optional) Configure lua language server for neovim
-                        local lua_opts = lsp_zero.nvim_lua_ls()
-                        require('lspconfig').lua_ls.setup(lua_opts)
+                    function(server_name) -- default handler (optional)
+                        require("lspconfig")[server_name].setup {
+                            capabilities = capabilities
+                        }
                     end,
-                    --jdtls = function ()
-                    --    require('jdtls').setup{
-                    --        java={
-                    --            format = {
-                    --                enabled= true
-                    --            }
-                    --        }
-                    --    }
-                    --end
+
+                    ["lua_ls"] = function()
+                        local lspconfig = require("lspconfig")
+                        lspconfig.lua_ls.setup {
+                            capabilities = capabilities,
+                            settings = {
+                                Lua = {
+                                    diagnostics = {
+                                        globals = { "vim", "it", "describe", "before_each", "after_each" },
+                                    }
+                                }
+                            }
+                        }
+                    end,
                 }
             })
         end
