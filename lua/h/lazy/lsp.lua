14db0f855fd708138358327773c10fd6ec233375
H17 <61067199+H1743322@users.noreply.github.com>
2024-05-28 22:26:10 +0100

    fix: change diagnostic config

--- lua/h/lazy/lsp.lua
+++ lua/h/lazy/lsp.lua
@@ -125,14 +125,14 @@ return {
                     border = 'rounded',
                     focusable = false,
                     style = "minimal",
-                    source = true,
+                    source = "if_many",
 
                 },
             })
             vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
                 {
                     signs = true,
-                    underline = false,
+                    underline = true,
                     virtual_text = {
                         spacing = 5,
                         severity = {
