2f71e169c933ba2ede67a589a710f9e4c4db1d1a
H17 <61067199+H1743322@users.noreply.github.com>
2024-06-23 19:40:12 +0100

    feat: add keymap K for lsp

--- lua/h/autocmd.lua
+++ lua/h/autocmd.lua
@@ -67,6 +67,7 @@ autocmd('LspAttach', {
         local opts = { buffer = event.buf }
         vim.keymap.set("n", "gd", function() require('telescope.builtin').lsp_definitions() end, opts)
         vim.keymap.set("n", "gr", function() require('telescope.builtin').lsp_references() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
         vim.keymap.set("n", "gs", function() vim.diagnostic.signature_help() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
