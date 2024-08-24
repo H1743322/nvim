6cb99af448f23175217584486bf7cab5fd21e4eb
H17 <61067199+H1743322@users.noreply.github.com>
2024-08-24 01:28:27 +0100

    fix: signature help

--- lua/h/autocmd.lua
+++ lua/h/autocmd.lua
@@ -71,7 +71,7 @@ autocmd('LspAttach', {
         vim.keymap.set("n", "gi", function() require('telescope.builtin').lsp_implementations() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "gs", function() vim.diagnostic.signature_help() end, opts)
+        vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
