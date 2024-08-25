aa9974f5a64ca6edb73ece2cbe03106dc3c2e74a
H17 <61067199+H1743322@users.noreply.github.com>
2024-08-25 23:57:17 +0100

    feat(lualine): center filename

--- lua/h/lazy/lualine.lua
+++ lua/h/lazy/lualine.lua
@@ -31,7 +31,7 @@ return {
                             info = 'I:',
                             hint = 'H:' },
                     } },
-                lualine_c = { { 'filename', path = 1 } },
+                lualine_c = { { "%=", separator = { right = "" } }, { 'filename', path = 1, } },
                 lualine_x = { 'encoding', 'fileformat', 'filetype' },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
