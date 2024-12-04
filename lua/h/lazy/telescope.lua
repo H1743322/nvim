d11b132c0089d086aab6de26eb31017a9619444a
H17 <61067199+H1743322@users.noreply.github.com>
2024-12-04 22:33:28 +0000

    style: remove desc and wrap results

--- lua/h/lazy/telescope.lua
+++ lua/h/lazy/telescope.lua
@@ -12,6 +12,7 @@ return {
         local builtin = require('telescope.builtin')
         require "telescope".setup {
             defaults = {
+                wrap_results = true,
                 preview = {
                     treesitter = false
                 },
@@ -27,7 +28,8 @@ return {
                 layout_config = {
                     horizontal = {
                         -- width = 0.9, height = 0.9,
-                        preview_cutoff = 90, preview_width = 0.6 },
+                        preview_cutoff = 90, preview_width = 0.6
+                    },
                 },
             },
             pickers = {
@@ -60,35 +62,31 @@ return {
             require("telescope.builtin").find_files(require("telescope.themes").get_dropdown({
                 previewer = true,
             }))
-        end, { desc = "[F]ind [F]iles" })
-
+        end)
         --vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<C-p>', function()
             require("telescope.builtin").git_files(require("telescope.themes").get_dropdown({
                 previewer = true,
                 windblend = 10,
             }))
-        end, { desc = "Search git files" })
-
+        end)
         vim.keymap.set('n', '<leader>fs', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
-
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags)
+        vim.keymap.set('n', '<leader>fb', builtin.buffers)
         vim.keymap.set('n', '<leader>fg', builtin.live_grep)
         vim.keymap.set('n', '<leader>frr', builtin.lsp_references)
-        vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
+        vim.keymap.set('n', '<leader>fw', builtin.grep_string)
         vim.keymap.set('n', '<leader>fW', function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
-
         vim.keymap.set('n', '<leader>/', function()
             require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
                 previewer = false,
             }))
-        end, { desc = "[/] Fuzzily search in current buffer]" })
+        end)
 
         --require('telescope').load_extension('fzf')
     end
