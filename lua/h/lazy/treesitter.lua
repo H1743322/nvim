8a259ef6f39a83d7c292f21b9ca1f07a18b3c254
H17 <61067199+H1743322@users.noreply.github.com>
2025-02-19 23:56:07 +0000

    feat: xml tab size 2

--- lua/h/lazy/treesitter.lua
+++ lua/h/lazy/treesitter.lua
@@ -39,7 +39,7 @@ return {
                     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
                     if ok and stats and stats.size > max_filesize then
                         vim.notify(
-                            "File larger than 150KB treesitter disabled for performance",
+                            "File larger than 1MB treesitter disabled for performance",
                             vim.log.levels.WARN,
                             { title = "Treesitter" }
                         )
