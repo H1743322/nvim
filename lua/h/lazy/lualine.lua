8b1f9f1f0818197a44eb1b80a6b4b7eb735b4209
H17 <61067199+H1743322@users.noreply.github.com>
2025-04-03 20:37:02 +0100

    fix: function deprecated

--- lua/h/lazy/lualine.lua
+++ lua/h/lazy/lualine.lua
@@ -19,7 +19,7 @@ return {
         local function get_active_clients()
             local msg = ''
             local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-            local clients = vim.lsp.get_active_clients()
+            local clients = vim.lsp.get_clients()
             if next(clients) == nil then
                 return msg
             end
