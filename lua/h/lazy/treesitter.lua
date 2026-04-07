2d40789cb8245bd7a7cf7f1da88bd2d3cf8d0cef
H <61067199+H1743322@users.noreply.github.com>
2026-04-07 20:30:36 +0100

    fix: highlight

--- lua/h/lazy/treesitter.lua
+++ lua/h/lazy/treesitter.lua
@@ -4,58 +4,13 @@ return {
     event = { "BufEnter" },
     branch = 'main',
     lazy = false,
-    -- TODO: fix for neovim v0.12.x
+    -- TODO: max file size
     config = function()
-        require 'nvim-treesitter'.setup {
-
-            ensure_installed = {
-                "javascript",
-                "typescript",
-                "lua",
-                "c",
-                "go",
-                "bash"
-            },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            indent = {
-                enable = true,
-            },
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-
-                --
-                disable = function(lang, buf)
-                    if lang == "html" then
-                        return true
-                    end
-
-                    local max_filesize = 1 * 1024 * 1024
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        vim.notify(
-                            "File larger than 1MB treesitter disabled for performance",
-                            vim.log.levels.WARN,
-                            { title = "Treesitter" }
-                        )
-                        return true
-                    end
-                end,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-        }
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                pcall(vim.treesitter.start)
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
     end
 }
